9244d5f32a76b466b10f793c04e411b417bb01c8
Krnsand <krnsandin@gmail.com>
2025-05-19 13:30:49 +0200

    Change new hero image for test

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import { db } from "@/prisma/db";
-import { Box } from "@mui/material";
-import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid2";
-import Image from "next/image";
-import BuyButton from "./components/buy-button";
-import HeroText from "./components/hero-text";
-import ProductCard from "./components/product-card";
+import { db } from '@/prisma/db';
+import { Box } from '@mui/material';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid2';
+import Image from 'next/image';
+import BuyButton from './components/buy-button';
+import HeroText from './components/hero-text';
+import ProductCard from './components/product-card';
 
 export default async function Home() {
   const products = await db.product.findMany();
@@ -13,21 +13,21 @@ export default async function Home() {
     <main>
       <Box
         sx={{
-          position: "relative",
-          width: "100%",
+          position: 'relative',
+          width: '100%',
           height: { xs: 400, lg: 300 },
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          color: "white",
-          overflow: "hidden",
-          "@media (max-width: 1280px)": {
-            height: 300,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          color: 'white',
+          overflow: 'hidden',
+          '@media (max-width: 1280px)': {
+            height: 600,
           },
         }}
       >
         <Image
-          src="https://www.pharmaciepolygone.com/media/cache/original/01/ba/4c462d8f8f67d017ccab9e6942e5.jpg"
+          src="https://images.pexels.com/photos/4352247/pexels-photo-4352247.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
           alt="Hero Image"
           layout="fill"
           objectFit="cover"
