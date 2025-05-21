3ab71fee4956a87b9b4bc82c5894a54bc85d6dff
ahmad mohammadi <ahmmoh0210@gmail.com>
2025-05-21 07:02:59 +0200

    updated images, title and discription on products

--- data/index.ts
+++ data/index.ts
@@ -5,21 +5,17 @@
 
 import { Product } from "@prisma/client";
 
-
-
-
-
 /* Lägg till era produkter här */
 export const products: Product[] = [
   {
     id: "1",
     articleNumber: "11",
     image:
-      "https://beautyofjoseon.com/cdn/shop/files/RiceMilkThumb_0003_LayerComp4_1.jpg?v=1736150232&width=900",
-    title: "Toner",
+      "https://media.royaldesign.se/1/tradition-drawn-hm3-stol-20?quality=80&w=1366",
+    title: "&Tradition Drawn HM3 Matbordsstol Valnöt",
     description:
-      "Denna toner är designad för att lugna och återfukta huden efter rengöring. Berikad med rismjölk och fermenterade ingredienser, hjälper den till att balansera hudens pH-nivå och ger en fräsch känsla. Den är perfekt för att förbereda huden för efterföljande hudvårdsprodukter",
-    price: 199,
+      "Drawn HM3-stolen är en klassisk dansk design från &Tradition och designad av Hvidt & Mølgaard 1956. Stolen är baserad på traditionella hantverkstekniker och är tillverkad av organiska material. Stolen finns i följande färger: oljad ek, tvålbehandlad ek, oljad valnöt och svartmålad samt har ett fast ryggstöd. Sitsen är handvävd av mer än 100 meter naturpappersnöre, vilket säkerställer en bekväm sittplats. Det är en tidlös stol som med sin enkelhet och eleganta design passar in i alla hem. Stolen är perfekt som matbordsstol och bidrar till en nordisk och stilren atmosfär",
+    price: 8230,
     categoryId: "sofa",
     stockQuantity: 1,
   },
@@ -27,60 +23,60 @@ export const products: Product[] = [
     id: "2",
     articleNumber: "22",
     image:
-      "https://beautyofjoseon.com/cdn/shop/files/1_ed366512-7a8b-481b-aff1-13ff67549ae7.png?v=1705211742&width=900",
-    title: "Refreshing Pore Mask",
+      "https://media.royaldesign.se/1/ferm-living-distinct-soffbord-travertin-100x55-cm-15?quality=80&w=2560",
+    title: "Distinct Coffee Table",
     description:
-      "En uppfriskande mask som minskar synligheten av porer och ger en renare, jämnare hudton. Masken innehåller naturliga ingredienser som hjälper till att kontrollera olja och förbättra hudens textur. Den passar bra för de med fet eller kombinerad hud som vill ha en klarare hud",
+      "Ursprungligen inspirerad av japansk minimalism utforskar Distinct Coffee Table - Travertine skapandet av komplexa strukturer baserade på ett enda element. Den minimalistiska konstruktionen skapar en kontrast till travertinens jordnära texturer. Med de naturliga och mångsidiga egenskaperna hos den sandfärgade kalkstenen tillför den obearbetadhet och påtaglighet som framhäver möbelstyckets arkitektoniska karaktär.",
     categoryId: "sofa",
     stockQuantity: 1,
-    price: 250,
+    price: 17185,
   },
   {
     id: "3",
     articleNumber: "33",
     image:
-      "https://beautyofjoseon.com/cdn/shop/files/1_8f4b1947-4539-4aff-b2f7-18192c23cbf8.png?v=1705211384&width=528",
-    title: "Glow Serum",
+      "https://image-resizing.booztcdn.com/warm-nordic-furniture/wnf13010801040_c2198870549.webp?has_grey=1&has_webp=1&version=1d2c8dae281b17e45717bd1f977392c4&size=source",
+    title: "Dining chair by EW Bach, Skovby, 1950s-60s, Denmark",
     description:
-      "Ett ljusgivande serum som hjälper till att ge huden en naturlig lyster. Formulerat med vitaminer och antioxidanter, förbättrar det hudens struktur och återfuktning. Perfekt för att ge trött och glåmig hud en fräsch och ungdomlig glöd",
+      "Set of 4 chairs manufactured in Denmark in the 1950s-60s by Skovby. The chairs are designed by EW Bach. The chairs are made of mahogany on the structure and veneered with rosewood on the backrest. The chairs have been fully restored and reupholstered by our team. The padding has been changed, and we choose an off-white fabric for these pieces. They are sold as a set of 4 and the price is per piece.",
     categoryId: "sofa",
     stockQuantity: 1,
-    price: 79,
+    price: 4650,
   },
   {
     id: "4",
     articleNumber: "44",
     image:
-      "https://beautyofjoseon.com/cdn/shop/files/1_c7df0b43-9352-46c1-8dac-dd0602014d6b.png?v=1705211262&width=1080",
-    title: "Essence Water",
+      "https://www.nordicnest.se/assets/blobs/tradition-little-petra-vb1-fatolj-inkl-pouf-atd1-vitoljad-ek-moonlight/516155-01_1_ProductImageMain_1-a177da6b5f.jpeg?preset=medium",
+    title: "FÅTÖLJ + PALL LITTLE PETRA",
     description:
-      "En näringsrik essence som ger intensiv återfuktning och hjälper till att bibehålla hudens elasticitet. Den innehåller en blandning av fermenterade ingredienser som hjälper till att förbättra hudens allmänna hälsa och ger en plumpande effekt",
+      "Fåtöljen Little Petra med tillhörande fotpall formgavs i slutet på 1930-talet av Viggo Boesen och vann tidigt både uppmärksamhet och utmärkelser. Little Petra är en av relativt få verk formgivna av Viggo Boesen som kom att förknippas med danska funkis-stilen. Little Petra är en robust men samtidigt nätt fåtölj som passar i alla varierande omgivningar. Med sin öppna, inbjudande och låga loungestil är den det självklara valet för en mysig hemmakväll. Säljs här i fyra olika utföranden med tillhörande fotpall. Välj mellan fårskinn Sahara samt Moonlight med benstativ i vitoljad ek eller valnöt. Finns även i fler utföranden för beställning i butik.",
     categoryId: "sofa",
     stockQuantity: 1,
-    price: 79,
+    price: 66410,
   },
   {
     id: "5",
     articleNumber: "55",
     image:
-      "https://beautyofjoseon.com/cdn/shop/files/1_de2af4bd-f530-45da-88bd-8710eb65e937.png?v=1705211261&width=3000",
-    title: "Cleansing Oil",
+      "https://media.royaldesign.se/1/tradition-ita-bord-18?quality=80&w=1080",
+    title: "Ita OS2 matbord, 260 x 95 cm, ek",
     description:
-      "En mild men effektiv rengöringsolja som löser upp smink och orenheter utan att torka ut huden. Oljan omvandlas till en mjuk emulsion vid kontakt med vatten och lämnar huden ren, återfuktad och silkeslen utan att lämna en fet hinna",
+      "Matbordet Ita OS2 från &Tradition förkroppsligar Japandi-andan genom att blanda det bästa från skandinavisk och japansk design. Ita-bordet passar perfekt i både privata och offentliga utrymmen, och fungerar utmärkt som matbord i hemmet eller på restauranger, som skrivbord på kontor eller som blickfång i mötesrum. Bordets minimalistiska design fängslar med sin rena funktionalitet och sin fridfulla harmoni. Ita-kollektionen, som designats av det Köpenhamnsbaserade OEO Studio, består av en rad olika träbord och bänkar som passar in i olika typer av inredningar. Den tidlösa serien ger en känsla av balans och diskret elegans i alla miljöer",
     categoryId: "sofa",
     stockQuantity: 1,
-    price: 230,
+    price: 29840,
   },
   {
     id: "6",
     articleNumber: "66",
     image:
-      "https://beautyofjoseon.com/cdn/shop/files/1-NEW_1.png?v=1736151688&width=1080",
-    title: "Dynasty Cream",
+      "https://content.cylindo.com/api/v2/4608/products/161503/frames/1/161503.webp?size=1024&feature=1:261_DANNY_CREAM&feature=LEGS:Walnut_Natural",
+    title: "Sigrid",
     description:
-      "En lyxig ansiktskräm som erbjuder djup återfuktning och näring till huden. Med ingredienser som ginseng och andra naturliga extrakt hjälper den till att stimulera hudens återuppbyggnad och minskar synliga tecken på åldrande, såsom fina linjer och rynkor",
+      "Sigrid är en klassisk möbel med ett mjukt, subtilt nedtonat uttryck som samtidigt utstrålar ren elegans. Designen är tidlös med vinklade armstöd och massiva träben, där kuddarnas enkelhet understryker designens strama formspråk. Vega Sand Dune är en klassisk och tidlös textil med en enfärgad vävstruktur och en slät naturlig look och känsla. Den har fördelen av att vara tillverkad av en blandning av polyester och akryl, vilket ger hög slitstyrka och ljusbeständighet. Detta säkerställer lång fysisk hållbarhet och gör den lätt att underhålla. Färgen är en verklig klassiker med en blandning av beige, grå och offwhite garn, och de naturliga färgerna framhäver den övergripande naturliga looken. Textilfärgen uppfyller alla OEKO-TEX® 100-standarder.",
     categoryId: "sofa",
     stockQuantity: 1,
-    price: 399,
+    price: 9999,
   },
 ];
