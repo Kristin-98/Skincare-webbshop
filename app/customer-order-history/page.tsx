3883e7b5d15fd92a78c913cbab63d4eb7c788220
Kristin-98 <kristi98_s@hotmail.se>
2025-05-22 13:34:29 +0200

    Co-authored-by: borghanna <borghanna@users.noreply.github.com> Co-authored-by: David Jensen <Tsourdox@users.noreply.github.com>

--- app/customer-order-history/page.tsx
+++ app/customer-order-history/page.tsx
@@ -1,6 +1,20 @@
+import { db } from "@/prisma/db";
 import { Box, Typography } from "@mui/material";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { auth } from "../auth";
 
-export default function CustomerOrderHistory() {
+export default async function CustomerOrderHistory() {
+  const session = await auth.api.getSession({
+    headers: await headers(), // you need to pass the headers object.
+  });
+  if (!session) redirect("/signin");
+
+  const orders = await db.order.findMany({
+    where: { customerId: session.user.id },
+    // include: { orderRows: true, shippingAdress: true },
+  });
+
   return (
     <Box
       sx={{
@@ -24,42 +38,44 @@ export default function CustomerOrderHistory() {
         Orderhistorik
       </Typography>
 
-      <Box
-        sx={{
-          width: "100%",
-          maxWidth: { xs: "100%", sm: 500, md: 600 },
-          backgroundColor: "background.paper",
-          color: "primary.main",
-          p: { xs: 2, sm: 3 },
-          borderRadius: 2,
-          boxShadow: 2,
-        }}
-      >
-        <Typography
-          variant="body1"
+      {orders.map((order) => (
+        <Box
           sx={{
-            fontSize: { xs: "0.9rem", sm: "1rem" },
-            display: "flex",
-            justifyContent: "space-between",
+            width: "100%",
+            maxWidth: { xs: "100%", sm: 500, md: 600 },
+            backgroundColor: "background.paper",
+            color: "primary.main",
+            p: { xs: 2, sm: 3 },
+            borderRadius: 2,
+            boxShadow: 2,
           }}
         >
-          Ordernummer: 123456789{" "}
-          <Box
-            component="span"
+          <Typography
+            variant="body1"
             sx={{
-              display: "inline-block",
-              ml: 1,
-              px: 1.2,
-              py: 0.3,
-              backgroundColor: "primary.main",
-              color: "#fff",
-              borderRadius: 1,
+              fontSize: { xs: "0.9rem", sm: "1rem" },
+              display: "flex",
+              justifyContent: "space-between",
             }}
           >
-            Skickad
-          </Box>
-        </Typography>
-      </Box>
+            Ordernummer: {order.totalPrice}
+            <Box
+              component="span"
+              sx={{
+                display: "inline-block",
+                ml: 1,
+                px: 1.2,
+                py: 0.3,
+                backgroundColor: "primary.main",
+                color: "#fff",
+                borderRadius: 1,
+              }}
+            >
+              Skickad
+            </Box>
+          </Typography>
+        </Box>
+      ))}
     </Box>
   );
 }
