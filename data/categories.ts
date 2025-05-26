8db24564f6e2c9356bbf427a8a3df6094bdfe0e3
borghanna <hanna.borg@medieinstitutet.se>
2025-05-26 16:38:31 +0200

    import category model, seed categories successful

--- data/categories.ts
+++ data/categories.ts
@@ -1,25 +1,26 @@
-export type SeedCategory = {
-	name: string;
-	description: string;
-};
+import { Category } from "@prisma/client";
 
-export const categories: SeedCategory[] = [
+export const categories: Category[] = [
 	{
+		id: "chairs",
 		name: "Chairs",
 		description:
 			"Timeless chairs focused on comfort, craftsmanship, and natural materials. Scandinavian simplicity for every room.",
 	},
 	{
+		id: "tables",
 		name: "Tables",
 		description:
 			"Minimalist tables made of wood and stone with clean lines and functional design built to last.",
 	},
 	{
+		id: "sofas",
 		name: "Sofas",
 		description:
 			"Elegant sofas with soft shapes and muted tones. A balance of relaxation, luxury, and Nordic aesthetics.",
 	},
 	{
+		id: "sale",
 		name: "Sale",
 		description:
 			"Selected design favorites at a reduced price for a limited time.",
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,22 +1,31 @@
 import { products } from "@/data";
+import { categories } from "@/data/categories";
 import { db } from "./db";
 
 async function main() {
-  for (const { id, ...product } of products) {
-    await db.product.upsert({
-      where: { articleNumber: product.articleNumber },
-      update: product,
-      create: product,
-    });
-  }
+	for (const { id, ...product } of products) {
+		await db.product.upsert({
+			where: { articleNumber: product.articleNumber },
+			update: product,
+			create: product,
+		});
+	}
+
+	for (const category of categories) {
+		await db.category.upsert({
+			where: { id: category.id },
+			update: {},
+			create: category,
+		});
+	}
 }
 
 main()
-  .then(async () => {
-    await db.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await db.$disconnect();
-    process.exit(1);
-  });
+	.then(async () => {
+		await db.$disconnect();
+	})
+	.catch(async (e) => {
+		console.error(e);
+		await db.$disconnect();
+		process.exit(1);
+	});
