5476797e30de3bc578f11dfc4f5e1935e1803044
borghanna <hanna.borg@medieinstitutet.se>
2025-05-26 12:18:58 +0200

    category

--- data/categories.ts
+++ data/categories.ts
@@ -3,7 +3,22 @@ import { Category } from "@prisma/client";
 export const categories = [
 	{
 		name: "Stolar",
-		Description:
+		description:
 			"Tidlösa stolar med fokus på komfort, hantverk och naturliga material. Skandinavisk enkelhet för varje rum.",
+	},
+	{
+		name: "Bord",
+		description:
+			"Minimalistiska bord i trä och sten med rena linjer och funktionell design skapade för att hålla över tid.",
+	},
+	{
+		name: "Soffor",
+		description:
+			"Eleganta soffor med mjuka former och dova toner. En balans mellan avslappning, lyx och nordisk estetik.",
+	},
+	{
+		name: "Kampanjpris",
+		description:
+			"Utvalda designfavoriter till reducerat pris under en begränsad tid.",
 	},
 ];
