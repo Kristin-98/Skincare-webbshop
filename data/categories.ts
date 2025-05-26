1a1f0ff3e14e94e3a29afc72f1ce36a923974c3c
borghanna <hanna.borg@medieinstitutet.se>
2025-05-26 15:55:28 +0200

    comment out category.ts

--- data/categories.ts
+++ data/categories.ts
@@ -1,27 +1,27 @@
-export type SeedCategory = {
-	name: string;
-	description: string;
-};
+// export type SeedCategory = {
+// 	name: string;
+// 	description: string;
+// };
 
-export const categories: SeedCategory[] = [
-	{
-		name: "Chairs",
-		description:
-			"Timeless chairs focused on comfort, craftsmanship, and natural materials. Scandinavian simplicity for every room.",
-	},
-	{
-		name: "Tables",
-		description:
-			"Minimalist tables made of wood and stone with clean lines and functional design built to last.",
-	},
-	{
-		name: "Sofas",
-		description:
-			"Elegant sofas with soft shapes and muted tones. A balance of relaxation, luxury, and Nordic aesthetics.",
-	},
-	{
-		name: "Sale",
-		description:
-			"Selected design favorites at a reduced price for a limited time.",
-	},
-];
+// export const categories: SeedCategory[] = [
+// 	{
+// 		name: "Chairs",
+// 		description:
+// 			"Timeless chairs focused on comfort, craftsmanship, and natural materials. Scandinavian simplicity for every room.",
+// 	},
+// 	{
+// 		name: "Tables",
+// 		description:
+// 			"Minimalist tables made of wood and stone with clean lines and functional design built to last.",
+// 	},
+// 	{
+// 		name: "Sofas",
+// 		description:
+// 			"Elegant sofas with soft shapes and muted tones. A balance of relaxation, luxury, and Nordic aesthetics.",
+// 	},
+// 	{
+// 		name: "Sale",
+// 		description:
+// 			"Selected design favorites at a reduced price for a limited time.",
+// 	},
+// ];
