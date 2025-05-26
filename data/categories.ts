18bedfc568fbc21dfa535fb0e2e6c6a38f925b5c
borghanna <hanna.borg@medieinstitutet.se>
2025-05-26 12:11:07 +0200

    added category.ts

---
+++ data/categories.ts
@@ -0,0 +1,9 @@
+import { Category } from "@prisma/client";
+
+export const categories = [
+	{
+		name: "Stolar",
+		Description:
+			"Tidlösa stolar med fokus på komfort, hantverk och naturliga material. Skandinavisk enkelhet för varje rum.",
+	},
+];
