d646fc3105c4172b920bf6670e87e9c75fd29d7e
Krnsand <krnsandin@gmail.com>
2025-05-30 13:44:22 +0200

    Co-authored-by: Kristin-98 <Kristin-98@users.noreply.github.com> Co-authored-by: David Jensen <Tsourdox@users.noreply.github.com> Co-authored-by: borghanna <borghanna@users.noreply.github.com>

--- app/checkout/actions/process-checkout.tsx
+++ app/checkout/actions/process-checkout.tsx
@@ -11,16 +11,11 @@ export async function processCheckout(
   shippingAdress: Prisma.AdressCreateInput
 ) {
   const session = await auth.api.getSession({
-    headers: await headers(), // you need to pass the headers object.
+    headers: await headers(),
   });
 
-  if (!session) {
-    throw new Error("Not logged in");
-  }
-
-  if (cart.length === 0) {
-    throw new Error("Cart is empty");
-  }
+  if (!session) throw new Error("Not logged in");
+  if (cart.length === 0) throw new Error("Cart is empty");
 
   const address = await db.adress.create({
     data: shippingAdress,
@@ -31,19 +26,50 @@ export async function processCheckout(
     0
   );
 
-  const order = await db.order.create({
-    data: {
-      shippingAdressId: address.id,
-      totalPrice,
-      customerId: session.user.id,
-      orderRows: {
-        create: cart.map((item) => ({
-          productId: item.id,
-          quantity: item.quantity,
-          price: item.price, // vad händer om klinten sätter pris till 0??
-        })),
+  const productIds = cart.map((item) => item.id);
+  const existingProducts = await db.product.findMany({
+    where: { id: { in: productIds } },
+  });
+
+  if (existingProducts.length !== productIds.length) {
+    throw new Error("One or more products in cart do not exist");
+  }
+
+  // Transaktion
+  const order = await db.$transaction(async (tx) => {
+    const createdOrder = await tx.order.create({
+      data: {
+        shippingAdressId: address.id,
+        totalPrice,
+        customerId: session.user.id,
+        orderRows: {
+          create: cart.map((item) => ({
+            productId: item.id,
+            quantity: item.quantity,
+            price: item.price,
+          })),
+        },
       },
-    },
+    });
+
+    for (const item of cart) {
+      const product = await tx.product.findUnique({ where: { id: item.id } });
+
+      if (!product) throw new Error(`Product not found: ${item.id}`);
+
+      if (product.stockQuantity < item.quantity) {
+        throw new Error(
+          `Not enough stock for product ${product.title}. Available: ${product.stockQuantity}, requested: ${item.quantity}`
+        );
+      }
+
+      await tx.product.update({
+        where: { id: item.id },
+        data: { stockQuantity: product.stockQuantity - item.quantity },
+      });
+    }
+
+    return createdOrder;
   });
 
   return order.orderNumber;
