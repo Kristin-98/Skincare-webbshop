987c7a2d95b888fc8ae3d1af204dbe7626481b93
borghanna <hanna.borg@medieinstitutet.se>
2025-06-02 11:14:10 +0200

    logo in footer + change description metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,36 +10,37 @@ import { CartProvider } from "./providers/cart-provider";
 import theme from "./theme/theme";
 
 const poppins = Poppins({
-  subsets: ["latin"],
-  weight: "100",
+	subsets: ["latin"],
+	weight: "100",
 });
 
 export const metadata: Metadata = {
-  title: "Beauty of Joseon",
-  description: "Dina favoritprodukter online till en bra pris...",
+	title: "Scandi Furniture",
+	description:
+		"Discover timeless Scandinavian design — minimal, functional, and crafted to elevate every space.",
 };
 
 export default function RootLayout({ children }: PropsWithChildren) {
-  return (
-    <html lang="en">
-      <body className={poppins.className} style={{ margin: 0 }}>
-        <AppRouterCacheProvider>
-          <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <CartProvider>
-              <Box
-                sx={{
-                  bgcolor: "background.default",
-                }}
-              />
-              <Header />
-              {/* <SignInPage /> */}
-              {children}
-              <Footer />
-            </CartProvider>
-          </ThemeProvider>
-        </AppRouterCacheProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={poppins.className} style={{ margin: 0 }}>
+				<AppRouterCacheProvider>
+					<ThemeProvider theme={theme}>
+						<CssBaseline />
+						<CartProvider>
+							<Box
+								sx={{
+									bgcolor: "background.default",
+								}}
+							/>
+							<Header />
+							{/* <SignInPage /> */}
+							{children}
+							<Footer />
+						</CartProvider>
+					</ThemeProvider>
+				</AppRouterCacheProvider>
+			</body>
+		</html>
+	);
 }
