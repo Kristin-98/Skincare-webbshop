e38c57d1486b714b2c48ad06410c4987ee8792cd
Krnsand <krnsandin@gmail.com>
2025-06-02 14:39:40 +0200

    Translate to english, shorten titles of some products

--- data/index.ts
+++ data/index.ts
@@ -12,9 +12,9 @@ export const products: (Product & { categories: string[] })[] = [
 		articleNumber: "11",
 		image:
 			"https://cimmermann.uk/cdn/shop/products/tradition-drawn-chair-hm3-oak-1_2048x.jpg?v=1607527835",
-		title: "&Tradition Drawn HM3 Matbordsstol Valnöt",
+		title: "Drawn HM3 Dining Chair",
 		description:
-			"Drawn HM3-stolen är en klassisk dansk design från &Tradition och designad av Hvidt & Mølgaard 1956. Stolen är baserad på traditionella hantverkstekniker och är tillverkad av organiska material. Stolen finns i följande färger: oljad ek, tvålbehandlad ek, oljad valnöt och svartmålad samt har ett fast ryggstöd. Sitsen är handvävd av mer än 100 meter naturpappersnöre, vilket säkerställer en bekväm sittplats. Det är en tidlös stol som med sin enkelhet och eleganta design passar in i alla hem. Stolen är perfekt som matbordsstol och bidrar till en nordisk och stilren atmosfär",
+			"The Drawn HM3 chair is a classic Danish design from &Tradition, created by Hvidt & Mølgaard in 1956. The chair is based on traditional craftsmanship and made from organic materials. It is available in the following finishes: oiled oak, soap-treated oak, oiled walnut, and black-painted, with a fixed backrest. The seat is hand-woven with more than 100 meters of natural paper cord, ensuring a comfortable sitting experience. This timeless chair fits into any home with its simplicity and elegant design, making it perfect as a dining chair that adds a Nordic and stylish atmosphere.",
 		price: 8230,
 		categories: ["chairs", "sale"],
 		stockQuantity: 8,
@@ -26,7 +26,7 @@ export const products: (Product & { categories: string[] })[] = [
 			"https://media.royaldesign.se/1/ferm-living-distinct-soffbord-travertin-100x55-cm-15?quality=80&w=2560",
 		title: "Distinct Coffee Table",
 		description:
-			"Ursprungligen inspirerad av japansk minimalism utforskar Distinct Coffee Table - Travertine skapandet av komplexa strukturer baserade på ett enda element. Den minimalistiska konstruktionen skapar en kontrast till travertinens jordnära texturer. Med de naturliga och mångsidiga egenskaperna hos den sandfärgade kalkstenen tillför den obearbetadhet och påtaglighet som framhäver möbelstyckets arkitektoniska karaktär.",
+			"Originally inspired by Japanese minimalism, the Distinct Coffee Table - Travertine explores the creation of complex structures from a single element. Its minimalist construction contrasts beautifully with the earthy textures of travertine. With the natural and versatile characteristics of the sand-colored limestone, the piece highlights a raw tactility that enhances its architectural character.",
 		categories: ["tables"],
 		stockQuantity: 3,
 		price: 17185,
@@ -36,9 +36,9 @@ export const products: (Product & { categories: string[] })[] = [
 		articleNumber: "33",
 		image:
 			"https://www.nordicnest.se/assets/blobs/warm-nordic-cow-horn-stol-lader-ivory-valnotsstativ/Cow-Horn-stol-oak-2405059-t-ivory-leathe-78eb173c59.jpeg?preset=medium&dpr=2",
-		title: "Dining chair by EW Bach, Skovby, 1950s-60s, Denmark",
+		title: "Dining Chair by EW Bach",
 		description:
-			"Set of 4 chairs manufactured in Denmark in the 1950s-60s by Skovby. The chairs are designed by EW Bach. The chairs are made of mahogany on the structure and veneered with rosewood on the backrest. The chairs have been fully restored and reupholstered by our team. The padding has been changed, and we choose an off-white fabric for these pieces. They are sold as a set of 4 and the price is per piece.",
+			"Set of 4 chairs manufactured in Denmark during the 1950s–60s by Skovby. Designed by EW Bach, these chairs feature a mahogany frame with rosewood veneer on the backrest. The chairs have been fully restored and reupholstered by our team. The padding has been replaced and we selected an off-white fabric for the new look. Sold as a set of 4 — the price listed is per piece.",
 		categories: ["chairs"],
 		stockQuantity: 10,
 		price: 4650,
@@ -48,9 +48,9 @@ export const products: (Product & { categories: string[] })[] = [
 		articleNumber: "44",
 		image:
 			"https://www.nordicnest.se/assets/blobs/tradition-little-petra-vb1-fatolj-inkl-pouf-atd1-vitoljad-ek-moonlight/516155-01_1_ProductImageMain_1-a177da6b5f.jpeg?preset=medium",
-		title: "FÅTÖLJ + PALL LITTLE PETRA",
+		title: "Little Petra Lounge Chair + Ottoman",
 		description:
-			"Fåtöljen Little Petra med tillhörande fotpall formgavs i slutet på 1930-talet av Viggo Boesen och vann tidigt både uppmärksamhet och utmärkelser. Little Petra är en av relativt få verk formgivna av Viggo Boesen som kom att förknippas med danska funkis-stilen. Little Petra är en robust men samtidigt nätt fåtölj som passar i alla varierande omgivningar. Med sin öppna, inbjudande och låga loungestil är den det självklara valet för en mysig hemmakväll. Säljs här i fyra olika utföranden med tillhörande fotpall. Välj mellan fårskinn Sahara samt Moonlight med benstativ i vitoljad ek eller valnöt. Finns även i fler utföranden för beställning i butik.",
+			"The Little Petra lounge chair with matching ottoman was designed in the late 1930s by Viggo Boesen and quickly gained attention and awards. It’s one of the relatively few works by Boesen associated with the Danish functionalist style. Despite its robust appearance, Little Petra is compact and fits into a variety of interiors. With its open, inviting, and low-slung lounge style, it’s the perfect choice for a cozy evening at home. Sold here in four versions with matching ottoman. Choose between Sahara or Moonlight sheepskin with legs in white-oiled oak or walnut. Additional versions available for order in-store.",
 		categories: ["sofas"],
 		stockQuantity: 4,
 		price: 66410,
@@ -60,9 +60,9 @@ export const products: (Product & { categories: string[] })[] = [
 		articleNumber: "55",
 		image:
 			"https://media.royaldesign.se/1/tradition-ita-bord-18?quality=80&w=1080",
-		title: "Ita OS2 matbord, 260 x 95 cm, ek",
+		title: "Ita OS2 Dining Table, Oak",
 		description:
-			"Matbordet Ita OS2 från &Tradition förkroppsligar Japandi-andan genom att blanda det bästa från skandinavisk och japansk design. Ita-bordet passar perfekt i både privata och offentliga utrymmen, och fungerar utmärkt som matbord i hemmet eller på restauranger, som skrivbord på kontor eller som blickfång i mötesrum. Bordets minimalistiska design fängslar med sin rena funktionalitet och sin fridfulla harmoni. Ita-kollektionen, som designats av det Köpenhamnsbaserade OEO Studio, består av en rad olika träbord och bänkar som passar in i olika typer av inredningar. Den tidlösa serien ger en känsla av balans och diskret elegans i alla miljöer",
+			"The Ita OS2 table from &Tradition embodies the Japandi spirit by blending the best of Scandinavian and Japanese design. Perfect for both private and public spaces, the table works beautifully as a dining table in a home or restaurant, a desk in an office, or a centerpiece in a meeting room. Its minimalist design captivates with pure functionality and serene harmony. Designed by Copenhagen-based OEO Studio, the Ita collection includes a range of wooden tables and benches suited for various interiors. This timeless series adds balance and understated elegance to any space. 260 x 95 cm.",
 		categories: ["tables", "sale"],
 		stockQuantity: 1,
 		price: 29840,
@@ -74,7 +74,7 @@ export const products: (Product & { categories: string[] })[] = [
 			"https://image.hm.com/assets/hm/11/83/11832aa11272bb7d8aa3dc9d4bf8f8b2597389f2.jpg?imwidth=768",
 		title: "Sigrid",
 		description:
-			"Sigrid är en klassisk möbel med ett mjukt, subtilt nedtonat uttryck som samtidigt utstrålar ren elegans. Designen är tidlös med vinklade armstöd och massiva träben, där kuddarnas enkelhet understryker designens strama formspråk. Vega Sand Dune är en klassisk och tidlös textil med en enfärgad vävstruktur och en slät naturlig look och känsla. Den har fördelen av att vara tillverkad av en blandning av polyester och akryl, vilket ger hög slitstyrka och ljusbeständighet. Detta säkerställer lång fysisk hållbarhet och gör den lätt att underhålla. Färgen är en verklig klassiker med en blandning av beige, grå och offwhite garn, och de naturliga färgerna framhäver den övergripande naturliga looken. Textilfärgen uppfyller alla OEKO-TEX® 100-standarder.",
+			"Sigrid is a classic piece of furniture with a soft, subtly muted expression that radiates pure elegance. The design is timeless, featuring angled armrests and solid wooden legs, while the simple cushions reinforce the strict design language. The Vega Sand Dune fabric is a classic and timeless textile with a monochrome weave structure and a smooth natural look and feel. It's made from a blend of polyester and acrylic, providing excellent durability and lightfastness, ensuring long-lasting performance and easy maintenance. The color is a true classic, blending beige, grey, and off-white yarns to highlight its natural look. The fabric meets all OEKO-TEX® 100 standards.",
 		categories: ["sofas"],
 		stockQuantity: 1,
 		price: 9999,
