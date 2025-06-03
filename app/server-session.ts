1893fdc590491e265162c08949d352ea3f6a091c
borghanna <hanna.borg@medieinstitutet.se>
2025-06-03 14:43:37 +0200

    darkmode logo in footer "use server" is serversession

--- app/server-session.ts
+++ app/server-session.ts
@@ -1,15 +1,16 @@
+"use server";
 
 import { auth } from "@/app/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
 export async function getServerSession() {
-  return auth.api.getSession({ headers: await headers() });
+	return auth.api.getSession({ headers: await headers() });
 }
 
 export async function assertIsAdmin() {
-  const session = await getServerSession();
-  if (!session?.user.isAdmin) {
-    redirect("/permission-denied");
-  }
+	const session = await getServerSession();
+	if (!session?.user.isAdmin) {
+		redirect("/permission-denied");
+	}
 }
