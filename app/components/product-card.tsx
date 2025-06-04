5818cb6c8a54bc8e72bce8d0b83081903f9b67ee
Krnsand <krnsandin@gmail.com>
2025-06-04 14:23:28 +0200

    Fix spacing error on category page

--- app/components/product-card.tsx
+++ app/components/product-card.tsx
@@ -3,44 +3,45 @@ import { Product } from "@prisma/client";
 import Link from "next/link";
 
 interface Props {
-	product: Product;
-	children?: React.ReactNode;
+  product: Product;
+  children?: React.ReactNode;
 }
 
 export default function ProductCard({ product, children }: Props) {
-	return (
-		<Card sx={{ maxWidth: 345, marginTop: 4 }}>
-			<Link
-				href={`/product/${product.articleNumber}/${encodeURIComponent(
-					product.title
-				)}`}
-			>
-				<CardMedia
-					sx={{ height: 300 }}
-					component="img"
-					image={product.image}
-					key={product.id}
-				></CardMedia>
-			</Link>
-			<Typography variant="h6" sx={{ boxSizing: "border-box", paddingLeft: 2 }}>
-				{product.title}
-			</Typography>
+  return (
+    <Card sx={{ maxWidth: 345, marginTop: 4 }}>
+      <Link
+        href={`/product/${product.articleNumber}/${encodeURIComponent(
+          product.title
+        )}`}
+      >
+        <CardMedia
+          sx={{ height: 300 }}
+          component="img"
+          image={product.image}
+          key={product.id}
+        ></CardMedia>
+      </Link>
+      <Typography variant="h6" sx={{ boxSizing: "border-box", paddingLeft: 2 }}>
+        {product.title}
+      </Typography>
 
-			<Box
-				sx={{
-					display: "flex",
-					justifyContent: "space-between",
-					alignItems: "center",
-				}}
-			>
-				<Typography
-					variant="body1"
-					sx={{ boxSizing: "border-box", paddingLeft: 2 }}
-				>
-					Price: {product.price} kr
-				</Typography>
-				{children}
-			</Box>
-		</Card>
-	);
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          paddingBottom: 2,
+        }}
+      >
+        <Typography
+          variant="body1"
+          sx={{ boxSizing: "border-box", paddingLeft: 2 }}
+        >
+          Price: {product.price} kr
+        </Typography>
+        {children}
+      </Box>
+    </Card>
+  );
 }
